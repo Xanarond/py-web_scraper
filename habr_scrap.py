5fe188e888f31a9d7507cfe5c98fe27d96367800
Vlad Kostyukhin <kostyukhin.v@EU.corp.samsungelectronics.net>
2021-09-29 16:40:29 +0300

    Add json parsing

--- habr_scrap.py
+++ habr_scrap.py
@@ -1,42 +1,62 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
 
-url = 'https://career.habr.com/vacancies?page=1'
-link = requests.get(url)
-# print(link.status_code)
-html = link.content
-soup = BeautifulSoup(html, 'lxml')
-cards = soup.find_all('div', class_='vacancy-card')
-section = soup.find('div', class_='paginator')
-href = section.find_all('a')
-
-page_num = []
-for num in href:
-    a = num.get_text()
-    page_num.append(a)
-
-bad_chars = ['Next ›', '‹ Prev']
-ready_set = list(set(page_num))
-test_string = filter(lambda w: w not in bad_chars, ready_set)
-max_href = max(list(test_string))
-N = int(max_href)
-urls = list(range(1, N + 1))
-
-
-for num in urls:
-    page_num = '?page=' + str(num)
-    new_url = url.replace('?page=1', page_num)
-    print(new_url)
-    response = requests.get(new_url)
-    content = response.content
-    soup = BeautifulSoup(content, 'lxml')
+def habr_scrap():
+    url = 'https://career.habr.com/vacancies?page=1'
+    link = requests.get(url)
+    # print(link.status_code)
+    html = link.content
+    soup = BeautifulSoup(html, 'lxml')
     cards = soup.find_all('div', class_='vacancy-card')
-    for section in cards:
-        date = section.find('div', class_='vacancy-card__company').text
-        company = section.find('div', class_='vacancy-card__company').text
-        title = section.find('div', class_='vacancy-card__title').text
-        location = section.find('div', class_='vacancy-card__meta').text.replace(' · ', ', ')
-        salary = section.find('div', class_='vacancy-card__salary').text
-        skills = section.find('div', class_='vacancy-card__skills').text.replace(' · ', ', ')
-        print(f' Компания: {company}\n Вакансия: {title}\n Расположение: {location}\n Навыки: {skills}\n ЗП: {salary}\n ')
+    section = soup.find('div', class_='paginator')
+    href = section.find_all('a')
+
+    page_num = []
+    for num in href:
+        a = num.get_text()
+        page_num.append(a)
+
+    bad_chars = ['Next ›', '‹ Prev']
+    ready_set = list(set(page_num))
+    test_string = filter(lambda w: w not in bad_chars, ready_set)
+    max_href = max(list(test_string))
+    N = int(max_href)
+    urls = list(range(1, N + 1))
+    direct = []
+    for num in urls:
+        page_num = '?page=' + str(num)
+        new_url = url.replace('?page=1', page_num)
+        print(new_url)
+        response = requests.get(new_url)
+        content = response.content
+        soup = BeautifulSoup(content, 'lxml')
+        cards = soup.find_all('div', class_='vacancy-card')
+        page_str = 'Page:' + str(num)
+        direct.append(page_str)
+        for section in cards:
+            date = section.find('div', class_='vacancy-card__date').text
+            company = section.find('div', class_='vacancy-card__company').text
+            title = section.find('div', class_='vacancy-card__title').text
+            location = section.find('div', class_='vacancy-card__meta').text.replace(' · ', ', ')
+            salary = section.find('div', class_='vacancy-card__salary').text
+            skills = section.find('div', class_='vacancy-card__skills').text.replace(' · ', ', ')
+            more_info = 'https://career.habr.com/' + section.find('a', class_='vacancy-card__icon-link').get('href')
+            # print(
+            #     f' Компания: {company}\n Вакансия: {title}\n Расположение: {location}\n Навыки: {skills}\n ЗП: {salary}\n Подробности: {more_info}\n ')
+            about = {
+                'date': date,
+                'company': company,
+                'title': title,
+                'location': location,
+                'salary': salary,
+                'skills': skills,
+                'more_info': more_info
+            }
+            direct.append(about)
+    return direct
+
+
+with open("habr.json", "w", encoding='utf-8') as write_file:
+    json.dump(habr_scrap(), write_file, indent=4, ensure_ascii=False, separators=(',', ': '))
--- relocate_scrap.py
+++ relocate_scrap.py
@@ -1,35 +1,54 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
-url = 'https://relocate.me/search/web-developer?page=1'
-link = requests.get(url)
-status = link.status_code
-content = link.content
-# print(status)
-soup = BeautifulSoup(content, 'lxml')
-pages = soup.find('span', class_='pages hide-for-small-only')
-last_url = pages.find_all('a')[-1].get_text()
-N = int(last_url)
-urls = list(range(1, N + 1))
 
-for num in urls:
-    page_num = '?page=' + str(num)
-    new_url = url.replace('?page=1', page_num)
-    print(new_url)
-    response = requests.get(new_url)
-    content = response.content
+def scrap_relocate():
+    url = 'https://relocate.me/search/web-developer?page=1'
+    link = requests.get(url)
+    status = link.status_code
+    content = link.content
+    # print(status)
     soup = BeautifulSoup(content, 'lxml')
-    cards = soup.find_all('div', class_='jobs-list__job')
-    for inx, card in enumerate(cards, start=1):
-        page = page_num
-        title = card.find('div', class_='job__title').text.strip()
-        more_info = 'https://relocate.me/' + card.find('a').get('href')
-        title_info = title.split(" in ")
-        vacancy = title_info[0]
-        location = title_info[1]
-        job_company = card.find('div', class_='job__company').text.strip()
-        job_preview = card.find('p', class_='job__preview').text.strip()
-        skills = card.find('div', class_='job__tags_wrapper').text.strip().replace('\n', ', ')
-        # print(page)
-        print(f'{inx}:\n Компания: {job_company}\n Вакансия: {title}\n Расположение: {location}\n Навыки: {skills}\n '
-              f'Подробности:{more_info}')
+    pages = soup.find('span', class_='pages hide-for-small-only')
+    last_url = pages.find_all('a')[-1].get_text()
+    N = int(last_url)
+    urls = list(range(1, N + 1))
+    diction = []
+    for num in urls:
+        page_num = '?page=' + str(num)
+        new_url = url.replace('?page=1', page_num)
+        print(new_url)
+        response = requests.get(new_url)
+        content = response.content
+        soup = BeautifulSoup(content, 'lxml')
+        cards = soup.find_all('div', class_='jobs-list__job')
+        page_str = 'Page:' + str(num)
+        diction.append(page_str)
+        for inx, card in enumerate(cards, start=1):
+            page = page_num.replace('?page=', 'Page: ')
+            title = card.find('div', class_='job__title').text.strip()
+            more_info = 'https://relocate.me/' + card.find('a').get('href')
+            title_info = title.replace('\n', ' ').split(" in ")
+            vacancy = str(title_info[0])
+            location = str(title_info[1])
+            job_company = card.find('div', class_='job__company').text.strip()
+            job_preview = card.find('p', class_='job__preview').text.strip()
+            skills = card.find('div', class_='job__tags_wrapper').text.strip().replace('\n', ', ')
+            about = {
+                'vacancy': vacancy,
+                'location': location,
+                'job_company': job_company,
+                'job_preview': job_preview,
+                'skills': skills,
+                'more_info': more_info
+            }
+            diction.append(about)
+            # print(page)
+            # print(f'{inx}:\n Компания: {job_company}\n Вакансия: {vacancy}\n Расположение: {location}\n Навыки: {skills}\n '
+            #       f'Подробности: {more_info}')
+    return diction
+
+
+with open("relocate.json", "w", encoding='utf-8') as write_file:
+    json.dump(scrap_relocate(), write_file, indent=4, ensure_ascii=False, separators=(',', ': '))
